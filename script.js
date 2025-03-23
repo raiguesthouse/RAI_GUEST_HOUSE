4cdb165969b753cc3b25860e130316010702e61e
raiguesthouse <aakashrai@myyahoo.com>
2025-03-23 22:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ async function submitOrder() {
 
     try {
         // Vercel proxy pe order submit karo (POST request bhejo)
-        const response = await fetch('https://rai-guest-house-proxy-d1sgu0xv-raiguesthouses-projects.vercel.app/submit-order', {
+        const response = await fetch('https://rai-guest-house-proxy-d1sguox7v-raiguesthouses-projects.vercel.app/submit-order', {
     method: 'POST',
     body: JSON.stringify(orderData),
     headers: { 'Content-Type': 'application/json' },
